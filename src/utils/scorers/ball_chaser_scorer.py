8ab2fcd8eb23687293562cf72de559bf7ea62fc6
TrumanHeberle <trumanheberle@gmail.com>
2021-04-12 01:30:09 -0400

    removed unused imports

--- src/utils/scorers/ball_chaser_scorer.py
+++ src/utils/scorers/ball_chaser_scorer.py
@@ -1,5 +1,4 @@
 from utils.scorers.scorer import StateScorer
-import utils.vector as vector
 
 class Scorer(StateScorer):
     """Scores the state such that bots are rewarded for being closer to the ball
