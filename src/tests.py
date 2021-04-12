28cab07062f09f355bee490b8e1b43542c4cd6ec
TrumanHeberle <trumanheberle@gmail.com>
2021-04-12 18:02:55 -0400

    updated test suites to handle timeouts

--- src/tests.py
+++ src/tests.py
@@ -3,59 +3,53 @@ from os import listdir
 from sys import exc_info, modules
 from traceback import extract_tb
 from inspect import getmembers, isfunction
+from multiprocessing import Pool, TimeoutError
 
-def run_suite(cases, fname=None):
+TIMEOUT = 1 # seconds
+
+def run_case(case):
+    """Safely executes a test case and returns an error string on fail"""
+    try:
+        case()
+        return None
+    except Exception as err:
+        trace = extract_tb(exc_info()[2])[-1]
+        line = trace[1]
+        statement = trace[3]
+        reason = str(err)
+        return f"line {line} in statement '{statement}'"+(f"\n\t{reason}" if reason else "")
+
+def run_suite(cases, fname="unspecified"):
     """Runs a series of test cases and prints the results"""
-    # run test cases
-    passed = 0
-    results = []
-    for case in cases:
-        try:
-            case()
-            passed += 1
-            results.append(False)
-        except Exception as err:
-            trace = extract_tb(exc_info()[2])[-1]
-            line = trace[1]
-            statement = trace[3]
-            reason = str(err)
-            results.append((line,statement,reason))
-    # format results
-    header = "unit testing"
-    header += " ("+str(fname)+"): " if fname else ": "
-    header += str(passed)+"/"+str(len(results))
-    strs = []
-    nmax = 0
-    for i,r in enumerate(results):
-        log = [cases[i].__name__+":",cases[i].__doc__,None]
-        if not r:
-            log[0] = "- passed "+log[0]
-        else:
-            log[0] = "- failed "+log[0]
-            log[2] = "\tline {} in statement \"{}\"\n\t{}".format(*r)
-        strs.append(log)
-        if len(log[0])>nmax:
-            nmax = len(log[0])
+    # run test cases safely
+    with Pool() as pool:
+        results = [pool.apply_async(run_case,[case]) for case in cases]
+        for i,result in enumerate(results):
+            try:
+                results[i] = result.get(TIMEOUT)
+            except TimeoutError as err:
+                results[i] = f"timed out after {TIMEOUT} seconds"
+    # formatting functions
+    passed = sum(error is None for error in results)
+    status = lambda r: "failed" if r else "passed"
+    name = lambda i: cases[i].__name__
+    doc = lambda i: cases[i].__doc__ if cases[i].__doc__ else ""
+    reason = lambda r: f"\n\t{r}"
+    log_str = lambda i,r: (f"- {status(r)} {name(i)}:",f"{doc(i)}{reason(r) if r else ''}")
     # print results
-    print(header)
-    for rs in strs:
-        # print function info
-        if rs[1]:
-            print(rs[0].ljust(2+nmax)+rs[1])
-        else:
-            print(rs[0])
-        # print failed reason
-        if rs[2]:
-            print(rs[2])
+    print(f"unit testing ({fname}): {passed}/{len(results)}")
+    log_strs = [log_str(i,r) for i,r in enumerate(results)]
+    n = max([len(ls[0]) for ls in log_strs])+2
+    for l1,l2 in log_strs:
+        print(f"{l1.ljust(n)}{l2}")
 
 def get_suite(mname):
     """Returns a list of test cases for a given module"""
     cases = []
     try:
         __import__(mname)
-        for fname, f in getmembers(modules[mname]):
-            if isfunction(f) and fname[:4]=="test":
-                cases.append(f)
+        is_case = lambda f, fname: isfunction(f) and fname[:4]=="test"
+        cases = [f for fname, f in getmembers(modules[mname]) if is_case(f,fname)]
     except Exception as err:
         print(err)
     finally:
@@ -68,8 +62,8 @@ if __name__=="__main__":
     suites = []
     # import test cases
     for fname in listdir(src):
-        mname, type = splitext(fname)
-        if isfile(join(src,fname)) and type==".py":
+        mname, fext = splitext(fname)
+        if isfile(join(src,fname)) and fext==".py":
             suite = get_suite("tests."+mname)
             if suite:
                 suites.append((suite,fname))
